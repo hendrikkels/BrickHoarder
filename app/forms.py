3d7af8c494ffbb5f5717b57fb8a562836752e98b
Hendrik van Heerden <53791068+hendrikkels@users.noreply.github.com>
2020-05-20 16:30:07 +0200

    Clear repo

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import InputRequired
-
-
-class SearchSetForm(FlaskForm):
-    # no = StringField('Set Number', validators=[InputRequired()])
-    no = StringField('Set Number', validators=[InputRequired()])
--- run.py
+++
@@ -1,6 +0,0 @@
-import socket
-socket.gethostbyname(socket.gethostname())
-
-from app import app
-
-app.run(host=socket.gethostbyname(socket.gethostname()), port='8080')
